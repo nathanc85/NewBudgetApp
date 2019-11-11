8b2ef8a419195b8bd196c032602b14c9fe861000
Nathan Costea <cnlnat@yahoo.com>
2019-11-11 02:59:41 -0500

    Created a budget service to hold and server the budgets data.

--- src/app/budgets/budget-list.component.ts
+++ src/app/budgets/budget-list.component.ts
@@ -1,3 +1,4 @@
+import { BudgetService } from './budget.service';
 import { Component, OnInit } from '@angular/core';
 import { IBudget } from './ibudget';
 
@@ -25,76 +26,18 @@ export class BudgetListComponent implements OnInit {
 
   filteredBudgets: IBudget[];
 
-  budgets: IBudget[] = [
-    {
-      budgetId: 1,
-      budgetName: 'Our November',
-      dateCreated: 'November 19, 2019',
-      lastModified: 'November 19, 2019',
-      budgetAmount: 500,
-      amountPlanned: 400,
-      amountUnplanned: 100,
-      amountSpent: 250,
-      amountRemaining: 250,
-      notes: 'Saving money for a car.',
-      planningRating: 4,
-      spendingRating: 2.5,
-      imageUrl: 'assets/images/bird1.png'
-    },
-    {
-      budgetId: 2,
-      budgetName: 'Best October',
-      dateCreated: 'October 19, 2019',
-      lastModified: 'October 19, 2019',
-      budgetAmount: 500,
-      amountPlanned: 300,
-      amountUnplanned: 200,
-      amountSpent: 200,
-      amountRemaining: 300,
-      notes: null,
-      planningRating: 3,
-      spendingRating: 2,
-      imageUrl: 'assets/images/bird2.png'
-    },
-    {
-      budgetId: 3,
-      budgetName: 'September',
-      dateCreated: 'September 19, 2019',
-      lastModified: 'September 19, 2019',
-      budgetAmount: 500,
-      amountPlanned: 500,
-      amountUnplanned: 0,
-      amountSpent: 500,
-      amountRemaining: 0,
-      notes: 'Going on vacation this month.',
-      planningRating: 5,
-      spendingRating: 5,
-      imageUrl: 'assets/images/bird3.png'
-    },
-    {
-      budgetId: 4,
-      budgetName: 'Gazzelle August',
-      dateCreated: 'August 19, 2019',
-      lastModified: 'August 19, 2019',
-      budgetAmount: 500,
-      amountPlanned: 300,
-      amountUnplanned: 200,
-      amountSpent: 300,
-      amountRemaining: 200,
-      notes: 'Gazzelle intensive',
-      planningRating: 3,
-      spendingRating: 3,
-      imageUrl: 'assets/images/bird4.png'
-    }
-  ];
+  budgets: IBudget[];
 
-  constructor() {
-    this.filteredBy = '';
-    this.filteredBudgets = this.budgets;
+  constructor(private budgetsService: BudgetService) {
+
   }
 
   ngOnInit() {
     console.log('Inside ngOnInit()');
+    this.budgets = this.budgetsService.getBudgets();
+
+    this.filteredBy = '';
+    this.filteredBudgets = this.budgets;
   }
 
   toggleImage() {
