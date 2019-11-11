735ce4a32306eb606dc7ecc25b67af5085424163
Nathan Costea <cnlnat@yahoo.com>
2019-11-10 20:15:09 -0500

    Added Bootstrap and Font-Awesome. Also added the BudgetsList Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BudgetsListComponent } from './budgets/budgets-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/budgets/budgets-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budgets-list',
+  templateUrl: './budgets-list.component.html',
+  styles: []
+})
+export class BudgetsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
